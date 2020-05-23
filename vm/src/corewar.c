5f5400dfd5108c3f72b677cfd5dba45df2d30a9f
mirageeeeee <dhardzoom@gmail.com>
2020-05-23 23:56:42 +0300

    start real

--- vm/src/corewar.c
+++ vm/src/corewar.c
@@ -67,37 +67,37 @@ t_cw	*arg_analize(char **av)
 	return (cw);
 }
 
-void	print_memory(const void *addr, size_t size)
-{
-	size_t			i;
-	size_t 			j;
-	unsigned char	*p;
-	char 			*str;
+// void	print_memory(const void *addr, size_t size)
+// {
+// 	size_t			i;
+// 	size_t 			j;
+// 	unsigned char	*p;
+// 	char 			*str;
 
-	str = "0123456789abcdef";
-	p = (unsigned char *)addr;
-	i = 0;
-	while (i < size)
-	{
-		j = 0;
-		while (j < 16 && i + j < size)
-		{
-			write(1, &str[(*(p + i + j)/16) % 16], 1);
-			write(1, &str[*(p + i + j) % 16], 1);
-				write(1, " ", 1);
-			j += 1;
-		}
-		while (j < 16)
-		{
-			write(1, "  ", 2);
-			if (j % 2)
-				write(1, " ", 1);
-			j++;
-		}
-		write(1, "\n", 1);
-		i += 16;
-	}
-}
+// 	str = "0123456789abcdef";
+// 	p = (unsigned char *)addr;
+// 	i = 0;
+// 	while (i < size)
+// 	{
+// 		j = 0;
+// 		while (j < 16 && i + j < size)
+// 		{
+// 			write(1, &str[(*(p + i + j)/16) % 16], 1);
+// 			write(1, &str[*(p + i + j) % 16], 1);
+// 				write(1, " ", 1);
+// 			j += 1;
+// 		}
+// 		while (j < 16)
+// 		{
+// 			write(1, "  ", 2);
+// 			if (j % 2)
+// 				write(1, " ", 1);
+// 			j++;
+// 		}
+// 		write(1, "\n", 1);
+// 		i += 16;
+// 	}
+// }
 
 int     main(int ac, char **av)
 {
