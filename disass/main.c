b5a21a2f4b3dcdcc54fa268da0bc69a847590652
Wezun4ik <tsupickov.ilya@gmail.com>
2020-06-20 18:16:04 +0300

    DISASS

--- disass/main.c
+++ disass/main.c
@@ -6,7 +6,7 @@
 /*   By: ilya <ilya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 21:43:14 by ilya              #+#    #+#             */
-/*   Updated: 2020/06/15 22:05:55 by ilya             ###   ########.fr       */
+/*   Updated: 2020/06/20 18:15:44 by ilya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,83 @@ char			*parse_bytecode(char *filename, int *size) //file to string
 	return (new_str);
 }
 
+int			get_identifier(int op_code)
+{
+	if (op_code == 3)
+		return (INDIRECT);
+	else if (op_code == 2)
+		return (DIRECT);
+	else
+		return (REGISTER);
+}
+
+int			get_type_by_command(int command)
+{
+	if (op_tab[command].valid_arg_types[0] & T_DIR)
+		return (DIRECT);
+	else if (op_tab[command].valid_arg_types[0] & T_IND)
+		return (INDIRECT);
+	else if (op_tab[command].valid_arg_types[0] & T_REG)
+		return (REGISTER);
+}
+
+t_command_list	*next_disass_command(char* file, int *i)
+{
+	t_command_list *ret = malloc(sizeof(t_command));
+	ret->next = NULL;
+	ret->command.is_label = 0;
+	ret->command.op_code = (int)file[*i];
+	(*i)++;
+	if (op_tab[ret->command.op_code - 1].arg_types_code)
+	{
+		ret->command.types[0] = get_identifier(ret->command.op_code >> 6 && 0x3);
+		ret->command.types[1] = get_identifier(ret->command.op_code >> 4 && 0x3);
+		ret->command.types[2] = get_identifier(ret->command.op_code >> 2 && 0x3);
+		(*i)++;
+	}
+	else
+		ret->command.types[0] = get_type_by_command(ret->command.op_code - 1);
+	for (int count = 0; count < op_tab[ret->command.op_code - 1].args_num; count++)
+	{
+		if (ret->command.types[count] == INDIRECT)
+		{
+			ret->command.values[count] = (int16_t*)file[*i];
+			*i += IND_SIZE;
+		}
+		else if (ret->command.types[count] == DIRECT)
+		{
+			ret->command.values[count] = (int32_t*)file[*i];
+			*i += DIR_SIZE;
+		}
+		else if (ret->command.types[count] == REGISTER)
+		{
+			ret->command.values[count] = (int32_t*)file[*i];
+			*i += REG_SIZE;
+		}
+	}
+	return (ret);
+}
+
+void	print_commands_to_file(t_command_list *list, int fd)
+{
+	while (list)
+	{
+		fprintf(fd, "%s ", op_tab[list->command.op_code - 1].op_name);
+		for (int i = 0; i < op_tab[list->command.op_code - 1].args_num; ++i)
+		{
+			if (list->command.types[i] == INDIRECT)
+				fprintf(fd, "%d", list->command.values[i]);
+			else if (list->command.types[i] == DIRECT)
+				fprintf(fd, "%c%d", DIRECT_CHAR, list->command.values[i]);
+			else if (list->command.types[i] == REGISTER)
+				fprintf(fd, "r%d", DIRECT_CHAR, list->command.values[i]);
+			if (i != op_tab[list->command.op_code - 1].args_num - 1)
+			fprintf(fd, ", ");
+		}
+		list = list->next;
+	}
+}
+
 void	disass(char *file, int fd, int size)
 {
 	fprintf(fd, NAME_CMD_STRING);
@@ -45,12 +122,14 @@ void	disass(char *file, int fd, int size)
 	fprintf(fd, COMMENT_CMD_STRING);
 	fprintf(fd, " %s\n", &file[12 + PROG_NAME_LENGTH]);
 	t_command_list *list = NULL;
-	while (file)
+	t_command_list *last = NULL;
+	int i = 16 + PROG_NAME_LENGTH + COMMENT_LENGTH;
+	while (i < size)
 	{
-		t_command_list	*next = get_next_command(&tokens_copy, &result, &last_label);
-		if (result.list == NULL)
+		t_command_list	*next = next_disass_command(file, &i);
+		if (list == NULL)
 		{
-			result.list = next;
+			list = next;
 			last = next;
 		}
 		else
@@ -59,6 +138,7 @@ void	disass(char *file, int fd, int size)
 			last = next;
 		}
 	}
+	print_commands_to_file(list, fd);
 }
 
 int main(int argc, char **argv)
